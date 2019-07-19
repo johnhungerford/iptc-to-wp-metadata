a40b9b2ec7bfb0ce6592e972066a66a61314b411
johnhungerford <hungerfordjustice@gmail.com>
2019-07-19 14:46:52 -0400

    cleaned up code; added comments

--- iptc-to-wp-metadata.php
+++ iptc-to-wp-metadata.php
@@ -8,25 +8,25 @@
  * Author URI: https://github.com/johnhungerford
  */
 
+// Bind plugin callback (below) to WP's 'add_attachment' hook, which fires when image is uploaded
 add_action( 'add_attachment', 'populate_img_desc_iptc_keywords');
 
+// This callback is the only action of this plugin
 function populate_img_desc_iptc_keywords($attachment_id) {
-    // Get EXIF data from the attachment file
+    // Get IPTC data from the attachment file
     $image = getimagesize( get_attached_file( $attachment_id ), $info );
     $iptc = iptcparse( $info['APP13'] );
     
+    // Only try to update WP metadata if 'Keywords' IPTC key ('2#025') is present
     if( isset( $iptc['2#025'] ) && is_array( $iptc['2#025'] ) ) {
-
-        // Last param is true to append these tags to existing tags,
-        // set it to false to replace existing tags
-        // See https://codex.wordpress.org/Function_Reference/wp_set_post_tags
-        $my_image_title = implode(' ', $iptc['2#025']);
-        $my_image_meta = array(
-            'ID'		    => $attachment_id,			// Specify the image (ID) to be updated
-			'post_title'	=> $my_image_title,		// Set image Title to sanitized title
-			'post_content'	=> $my_image_title,		// Set image Description (Content) to sanitized title
+        // Generate string from array of keywords, space-separated
+        $keywords_string = implode(' ', $iptc['2#025']);
+        $new_image_meta = array(
+            'ID'		    => $attachment_id,		// Specify the image (ID) to be updated
+			'post_title'	=> $keywords_string,	// Set image Title to string of keywords
+			'post_content'	=> $keywords_string,	// Set image Description (Content) to string of keywords
         );
         
-        wp_update_post( $my_image_meta );
+        wp_update_post( $new_image_meta );
    }
 }
