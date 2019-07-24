72b8f30124e5cb9167eb94039ea68c5f6dfd79e2
johnhungerford <hungerfordjustice@gmail.com>
2019-07-24 15:05:07 -0400

    now able to extract keywords from xmp if iptc is not available

--- iptc-to-wp-metadata.php
+++ iptc-to-wp-metadata.php
@@ -19,17 +19,43 @@ function populate_img_desc_iptc_keywords($attachment_id) {
     
     // Only try to update WP metadata if 'Keywords' IPTC key ('2#025') is present
     if( isset( $iptc['2#025'] ) && is_array( $iptc['2#025'] ) ) {
-        echo '<script>console.log("Found keywords in image!")</script>';
         // Generate string from array of keywords, space-separated
-        $keywords_string = implode(' ', $iptc['2#025']);
+        $keys = $iptc['2#025'];
+   } else {
+        $content = file_get_contents(get_attached_file($attachment_id));
+        $xmp_data_start = strpos($content, '<dc:subject>') + 12;
+        if ($xmp_data_start != FALSE) {
+            $xmp_data_end   = strpos($content, '</dc:subject>');
+            $xmp_data_length     = $xmp_data_end - $xmp_data_start;
+            $xmp_data       = substr($content, $xmp_data_start, $xmp_data_length);
+            $key_data_start = strpos($xmp_data, '<rdf:Seq>') + 9;
+            if ($key_data_start != FALSE) {
+                $key_data_end   = strpos($xmp_data, '</rdf:Seq>');
+                $key_data_length     = $key_data_end - $key_data_start;
+                $key_data       = substr($xmp_data, $key_data_start, $key_data_length);
+
+                $ctr = strpos($key_data, '<rdf:li>');
+                $keys = Array();
+                while($ctr != FALSE && $ctr < $key_data_length) {
+                    $ctr += 8;
+                    $endpos = strpos($key_data, '</rdf:li>', $ctr);
+                    if ($endpos == FALSE) break;
+
+                    array_push($keys, substr($key_data, $ctr, $endpos - $ctr));
+                    $ctr = $endpos + 10;
+                }
+            }
+        } 
+    }
+
+    if (isset($keys) && sizeof($keys) > 0) {
+        $keywords_string = implode(' ', $keys);
         $new_image_meta = array(
             'ID'		    => $attachment_id,		// Specify the image (ID) to be updated
-			'post_title'	=> $keywords_string,	// Set image Title to string of keywords
-			'post_content'	=> $keywords_string,	// Set image Description (Content) to string of keywords
+            'post_title'	=> $keywords_string,	// Set image Title to string of keywords
+            'post_content'	=> $keywords_string,	// Set image Description (Content) to string of keywords
         );
 
-        echo "<script>console.log('" . json_encode($new_image_meta) . "');</script>";
-        
         wp_update_post( $new_image_meta );
-   }
+    } 
 }
